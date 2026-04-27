0862ff73ba3d0fb993d445dbe785a8a3ee3902ad
lufiori <lufiori72@gmail.com>
2026-04-27 13:07:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+// CONFIG DO FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyBgqgHk8HPWexgN1oKGDjrQ4XmSN4MRKOI",
+  authDomain: "nutri-receitas-u92l41.firebaseapp.com",
+  projectId: "nutri-receitas-u92l41",
+  storageBucket: "nutri-receitas-u92l41.firebasestorage.app",
+  messagingSenderId: "384116468446",
+  appId: "1:384116468446:web:a63bde690aaf190ec6b04f"
+};
+
+// INICIAR
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const lista = document.getElementById("lista");
+
+// BUSCAR RECEITAS DO BANCO
+db.collection("receitas").get().then(snapshot => {
+  snapshot.forEach(doc => {
+    const r = doc.data();
+
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <strong>${r.nome}</strong><br>
+      🔥 ${r.calorias} kcal
+    `;
+
+    div.onclick = () => abrir(r);
+
+    lista.appendChild(div);
+  });
+});
+
+function abrir(r) {
+  document.getElementById("lista").classList.add("hidden");
+  document.getElementById("detalhes").classList.remove("hidden");
+
+  document.getElementById("titulo").innerText = r.nome;
+
+  document.getElementById("nutricao").innerText =
+    `🔥 ${r.calorias} kcal | 🥩 ${r.proteina}g | 🍞 ${r.carboidratos}g | 🧈 ${r.gordura}g`;
+
+  document.getElementById("ingredientes").innerText =
+    "Ingredientes: " + r.ingredientes;
+
+  document.getElementById("preparo").innerText =
+    "Preparo: " + r.preparo;
+}
+
+function voltar() {
+  document.getElementById("lista").classList.remove("hidden");
+  document.getElementById("detalhes").classList.add("hidden");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const receitas = [
   {
     nome: "Arroz com Ovo",
